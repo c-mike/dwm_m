870b99676aa8deab729f091e1f926fcdb7a382ef
Cláudio Michael <claudiomichael83@gmail.com>
2021-05-24 19:22:42 -0100

    some changes

--- install.sh
+++ install.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-# installation
-sudo make clean install
 
-# session configuration
+
+#                                           xprofile
+#echo "so na configura xprofile..."
+# if ![ -e ~/.xprofile ];
+# then
+#    cp .xprofile ~/
+# fi
+
+#                                           session configuration
+#echo "so na configura xsessions..."
 # sudo cp dwm.desktop /usr/share/xsessions/
 
-# echo $XDG_CURRENT_DESKTOP
+#                                           installation
+#echo "so na instala..."
+sudo make clean install
