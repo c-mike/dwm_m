9de2f2aebdeee23a9818cd4e12c6b8193c0dbb8d
Cláudio Michael <claudiomichael83@gmail.com>
2021-05-25 11:23:38 -0100

    some conf

--- install.sh
+++ install.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 
-
 #                                           xprofile
-#echo "so na configura xprofile..."
-# if ![ -e ~/.xprofile ];
-# then
-#    cp .xprofile ~/
-# fi
+echo "so na configura xprofile..."
+if ![ -e ~/.xprofile ];
+then
+   cp .xprofile ~/
+fi
 
 #                                           session configuration
-#echo "so na configura xsessions..."
-# sudo cp dwm.desktop /usr/share/xsessions/
+echo "so na configura xsessions..."
+if ![ -e /usr/share/xsessions/dwm.desktop ];
+then
+   sudo cp dwm.desktop /usr/share/xsessions/
+fi
 
 #                                           installation
 #echo "so na instala..."
